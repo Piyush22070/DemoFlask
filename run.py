4c2e53f0ba6235778805827db345d6fd564b6e47
Piyush22070 <piyushbhoyar6480@gmail.com>
2025-04-13 01:07:04 +0530

    updated run

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from app import create_app
-
-app = create_app()
+import os
+from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Get the PORT environment variable for deployment, default to 5000 locally
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)  # Change `debug=False` in production
