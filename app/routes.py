8665b1e5757ea691b37e05adf8cf24b97d9723fc
Piyush22070 <piyushbhoyar6480@gmail.com>
2025-04-13 01:25:48 +0530

    app issue

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from app import app
 from flask import request, jsonify
 import joblib
 
@@ -5,10 +6,7 @@ model = joblib.load("models/model.pkl")
 
 @app.route('/predict', methods=['POST'])
 def predict_route():
-    try:
-        data = request.get_json() 
-        input_features = data['input_features']
-        prediction = model.predict([input_features]) 
-        return jsonify({"prediction": int(prediction[0])})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
+    data = request.get_json()
+    input_features = data['input_features']
+    prediction = model.predict([input_features])
+    return jsonify({"prediction": int(prediction[0])})
